6896d5b2dcf74e496163885b93de1604da3da4e5
Developer <vilmebeaudelaire5@gmail.com>
2026-02-14 13:28:35 -0300

    fix(admin): patch jazzmin paginator format_html compatibility to prevent 500 on member changelist

--- apps/core/apps.py
+++ apps/core/apps.py
@@ -1,4 +1,5 @@
 from django.apps import AppConfig
+from django.utils.safestring import mark_safe
 
 
 class CoreConfig(AppConfig):
@@ -10,3 +11,20 @@ class CoreConfig(AppConfig):
         """Connecte les signals au démarrage de l'application."""
         # Import des signals pour les connecter
         import apps.core.signals  # noqa: F401
+
+        # Hotfix Jazzmin pagination: certaines versions appellent format_html sans args
+        # ce qui déclenche TypeError sur certaines versions de Django.
+        try:
+            from jazzmin.templatetags import jazzmin as jazzmin_tags
+
+            original_format_html = jazzmin_tags.format_html
+
+            def _safe_jazzmin_format_html(format_string, *args, **kwargs):
+                if not args and not kwargs:
+                    return mark_safe(format_string)
+                return original_format_html(format_string, *args, **kwargs)
+
+            jazzmin_tags.format_html = _safe_jazzmin_format_html
+        except Exception:
+            # Ne pas bloquer le démarrage si Jazzmin est indisponible
+            pass
