282923ff7e0d6034b94c293afc4e42f3391014f6
masterjay <59769164+beaudelaire1@users.noreply.github.com>
2026-08-27 17:12:11 -0300

    fix(ui): support multi-role checks in navigation

--- apps/core/templatetags/permission_tags.py
+++ apps/core/templatetags/permission_tags.py
@@ -1,6 +1,6 @@
 from django import template
 
-from apps.core.permissions import can_access_module
+from apps.core.permissions import can_access_module, has_role
 
 
 register = template.Library()
@@ -10,3 +10,9 @@ register = template.Library()
 def module_access(user, module):
     """Retourne True si l'utilisateur peut voir un module interne."""
     return can_access_module(user, module, internal=True)
+
+
+@register.simple_tag
+def role_access(user, *roles):
+    """Retourne True si l'utilisateur possède au moins un des rôles fournis."""
+    return has_role(user, *roles)
